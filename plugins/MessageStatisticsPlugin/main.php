48dfc31381e3dae18f7aabf53e40cef614c91266
Duncan Cameron <phplist@dcameron.me.uk>
2013-08-24 23:19:27 +0100

    Test that common plugin is installed and enabled

--- plugins/MessageStatisticsPlugin/main.php
+++ plugins/MessageStatisticsPlugin/main.php
@@ -29,6 +29,13 @@
  * @package   MessageStatisticsPlugin
  */
  
-include $plugins['CommonPlugin']->coderoot . 'Autoloader.php';
+$commonPlugin = isset($plugins['CommonPlugin']) ? $plugins['CommonPlugin'] : null;
+
+if (!($commonPlugin && $commonPlugin->enabled)) {
+    echo "phplist-plugin-common must be installed and enabled to use this plugin";
+    return;
+}
+
+include $commonPlugin->coderoot . 'Autoloader.php';
 
 CommonPlugin_Main::run(new MessageStatisticsPlugin_ControllerFactory());
