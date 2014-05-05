ff0922760e33787d21332c16eb7ed25984bf0260
Duncan Cameron <phplist@dcameron.me.uk>
2014-05-05 10:03:42 +0100

    Corrected line endings

--- plugins/MessageStatisticsPlugin/printfooter.tpl.php
+++ plugins/MessageStatisticsPlugin/printfooter.tpl.php
@@ -1,1 +1,10 @@
-<html>    <head></head>    <body>        <table width="100%" border="0">            <tr>                <td width="100%" style="text-align:right; font-size:9px;"><?php echo strftime("%c");?></td>            </tr>        </table>    </body></html>
+<html>
+    <head></head>
+    <body>
+        <table width="100%" border="0">
+            <tr>
+                <td width="100%" style="text-align:right; font-size:9px;"><?php echo strftime("%c");?></td>
+            </tr>
+        </table>
+    </body>
+</html>
